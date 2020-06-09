7816c02f323bf0bc08e06854b40ce0213ced4742
José Villegas <villegasjose.gg@gmail.com>
2020-06-09 00:08:49 -0400

    Star counter

--- Assets/Source/Gameplay/Score/ScorePointsConfiguration.cs
+++ Assets/Source/Gameplay/Score/ScorePointsConfiguration.cs
@@ -11,11 +11,18 @@ namespace TilesWalk.Gameplay.Score
 		[Range(1, 100)] [SerializeField] private int _pointsPerExtraSecond = 20;
 		[Range(1, 1000)] [SerializeField] private int _pointsPerExtraMove = 100;
 
+		[Range(0f, 1f)] [SerializeField] private float _oneStarRange = 0.25f;
+		[Range(0f, 1f)] [SerializeField] private float _twoStarRange = 0.65f;
+
 		public int PointsPerTile => _pointsPerTile;
 		public int ComboMultiplier => _comboMultiplier;
 
 		public int PointsPerExtraSecond => _pointsPerExtraSecond;
 
 		public int PointsPerExtraMove => _pointsPerExtraMove;
+
+		public float OneStarRange => _oneStarRange;
+
+		public float TwoStarRange => _twoStarRange;
 	}
 }
---
+++ Assets/Source/Navigation/UI/LevelTileStarsHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using TilesWalk.Gameplay.Score;
+using TilesWalk.Gameplay.Score.Installer;
+using TilesWalk.Navigation.Map;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace TilesWalk.Navigation.UI
+{
+	public class LevelTileStarsHandler : LevelNameRequireBehaviour
+	{
+		[SerializeField] private Sprite _starSprite;
+		[SerializeField] private Sprite _starFilledSprite;
+		[SerializeField] private Image[] _stars;
+
+		[Inject] private Dictionary<string, LevelScore> _scoreRecords;
+		[Inject] private ScorePointsConfiguration _scorePointsSettings;
+
+		protected override void OnTileMapFound()
+		{
+			if (LevelName?.Value == null) return;
+
+			if (!_scoreRecords.TryGetValue(LevelName.Value, out var levelScore))
+			{
+				return;
+			}
+
+			var ratio = (float) TileMap.Target / levelScore.Points.Highest;
+
+			for (int i = 0; i < _stars.Length; i++)
+			{
+				_stars[i].sprite = _starSprite;
+
+				if (i == 0 && ratio > _scorePointsSettings.OneStarRange)
+				{
+					_stars[i].sprite = _starFilledSprite;
+				}
+
+				if (i == 1 && ratio > _scorePointsSettings.TwoStarRange)
+				{
+					_stars[i].sprite = _starFilledSprite;
+				}
+
+				if (i == 2 && ratio >= 1f)
+				{
+					_stars[i].sprite = _starFilledSprite;
+				}
+			}
+		}
+	}
+}
