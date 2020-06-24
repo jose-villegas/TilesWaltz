944cfa38b22c46fcf1ef5f549825d5557dd7fac9
José Villegas <villegasjose.gg@gmail.com>
2020-06-24 15:26:08 -0400

    Level summary details UI changed

---
+++ Assets/Source/Map/UI/LevelCompletedToggle.cs
@@ -0,0 +1,26 @@
+﻿using TilesWalk.Building.Level;
+using TilesWalk.Gameplay.Score;
+using TilesWalk.General.Patterns;
+using TilesWalk.Map.Scaffolding;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace TilesWalk.Map.UI
+{
+	[RequireComponent(typeof(Toggle))]
+	public class LevelCompletedToggle : ObligatoryComponentBehaviour<Toggle>, ILevelNameRequire
+	{
+		[Inject] private GameScoresHelper _gameScoresHelper;
+
+		public ReactiveProperty<string> Name { get; } = new ReactiveProperty<string>();
+		public ReactiveProperty<LevelMap> Map { get; } = new ReactiveProperty<LevelMap>();
+
+		private void Awake()
+		{
+			Map.Subscribe(map => Component.isOn = map != null && _gameScoresHelper.IsCompleted(map)).AddTo(this);
+		}
+	}
+}
---
+++ Assets/Source/Map/UI/LevelTargetRecordLabel.cs
@@ -0,0 +1,28 @@
+﻿using TilesWalk.Building.Level;
+using TilesWalk.Extensions;
+using TilesWalk.General.Patterns;
+using TilesWalk.Map.General;
+using TilesWalk.Map.Scaffolding;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace TilesWalk.Map.UI
+{
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class LevelTargetRecordLabel : ObligatoryComponentBehaviour<TextMeshProUGUI>, ILevelNameRequire
+	{
+		[Inject] private MapProviderSolver _solver;
+
+		public ReactiveProperty<string> Name { get; } = new ReactiveProperty<string>();
+		public ReactiveProperty<LevelMap> Map { get; } = new ReactiveProperty<LevelMap>();
+
+		private void Awake()
+		{
+			_solver.InstanceProvider(gameObject);
+			Map.SubscribeToText(Component,
+				map => map != null ? _solver.Provider.Records[map.Id].Points.Highest.Localize() : "").AddTo(this);
+		}
+	}
+}
