17ffac2c6b1efab6b244b32d09eb915d0b66760f
José Villegas <villegasjose.gg@gmail.com>
2020-07-04 01:01:08 -0400

    Change settings

--- Assets/Source/GPGS/GPGSLeaderbardsHandler.cs
+++ Assets/Source/GPGS/GPGSLeaderbardsHandler.cs
@@ -11,8 +11,8 @@ namespace TilesWalk.GPGS
 			if (!PlayGamesPlatform.Instance.localUser.authenticated) return;
 
 			PlayGamesPlatform.Instance.ReportScore(totalScore,
-				GPGSIds.leaderboard_levels_score,
-				(success) => { Debug.Log("(leaderboard_levels_score) update: " + success); });
+				GPGSIds.leaderboard_all_levels_score,
+				(success) => { Debug.Log("(leaderboard_all_levels_score) update: " + success); });
 		}
 	}
 }
